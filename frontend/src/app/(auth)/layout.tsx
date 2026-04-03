7f84b15b118c16fafd4275c39ec3438e995b7ecc
NegiAbhijeet <abhijeetnegi.ab@gmail.com>
2026-04-03 11:39:25 +0530

    seperating backend from fronend

--- frontend/src/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import PublicRoute from '@/components/PublicRoute';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <PublicRoute>{children}</PublicRoute>;
-}
--- frontend/src/app/(protected)/layout.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import ProtectedRoute from '@/components/ProtectedRoute';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <ProtectedRoute>{children}</ProtectedRoute>;
-}
